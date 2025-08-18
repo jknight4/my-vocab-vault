3c4404734e7e6fd5ea5f9a053b7564cd07a26157
Jeremy <knightj0044@gmail.com>
2025-08-18 08:24:28 -0500

    more impl

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	kotlin("jvm") version "1.9.25"
 	kotlin("plugin.spring") version "1.9.25"
+	kotlin("plugin.noarg") version "1.9.25"
 	id("org.springframework.boot") version "3.5.4"
 	id("io.spring.dependency-management") version "1.1.7"
 }
@@ -26,12 +27,19 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation(platform("io.awspring.cloud:spring-cloud-aws-dependencies:3.4.0"))
+	implementation("io.awspring.cloud:spring-cloud-aws-starter")
+	implementation("io.awspring.cloud:spring-cloud-aws-starter-dynamodb")
 }
 
 kotlin {
 	compilerOptions {
 		freeCompilerArgs.addAll("-Xjsr305=strict")
 	}
+}
+
+noArg {
+	annotation("software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean")
 }
 
 tasks.withType<Test> {
