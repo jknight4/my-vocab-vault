e05cc4cbfd80530472bc5e5c1f78f25956402149
Jeremy <knightj0044@gmail.com>
2025-08-23 13:19:17 -0500

    more work

--- api/src/main/kotlin/xyz/knightj/my_vocab_vault/MyVocabVaultApplication.kt
+++ api/src/main/kotlin/xyz/knightj/my_vocab_vault/MyVocabVaultApplication.kt
@@ -1,13 +1,8 @@
 package xyz.knightj.my_vocab_vault
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
-
 @SpringBootApplication
-//@EnableJpaRepositories("xyz.knightj.my_vocab_vault.persistence.repositories")
-//@EntityScan("xyz.knightj.my_vocab_vault.persistence.entities")
 class MyVocabVaultApplication
 
 fun main(args: Array<String>) {
